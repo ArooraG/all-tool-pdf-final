1da6699ca7095b882244b4376f44322de0fa73c1
ArooraG <aroora424@gmail.com>
2025-09-26 10:54:05 -0700

    Add files via upload

--- build.sh
+++ build.sh
@@ -1,10 +1,19 @@
- #!/usr/bin/env bash
-    # exit on error
-    set -o errexit
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
 
-    # Install dependencies from requirements.txt
-    pip install -r requirements.txt
+# Install Python dependencies first
+pip install -r requirements.txt
 
-    # Install LibreOffice for Word/Excel to PDF conversion
-    apt-get update && apt-get install -y libreoffice && apt-get clean
-    
+# --- UPGRADED LIBREOFFICE INSTALLATION ---
+# Update package list and install the full version of LibreOffice along with extra fonts.
+# 'libreoffice-l10n-en-gb' helps with language packs.
+# 'fonts-takao-gothic' provides essential Japanese fonts.
+# 'fonts-noto' is a massive font collection from Google covering almost all languages.
+# This makes our converter much more powerful for international documents.
+apt-get update && apt-get install -y \
+    libreoffice \
+    libreoffice-l10n-en-gb \
+    fonts-takao-gothic \
+    fonts-noto \
+    && apt-get clean
